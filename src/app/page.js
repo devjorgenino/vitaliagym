021970eab7946310c763c6416ba715d5d52026c1
Jorge Niño <jorgenino.dev@gmail.com>
2026-01-05 18:42:26 -0400

    fix: fix redirect when login

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,19 @@
 import Login from "@/components/auth/Login.jsx";
 import useAuth from "@/hooks/useAuth.js";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const { user, loading } = useAuth();
   const router = useRouter();
 
+  useEffect(() => {
+    if (!loading && user) {
+      router.push("/dashboard");
+    }
+  }, [user, loading, router]);
+
   if (!loading && user) {
-    router.push("/dashboard");
     return null;
   }
 
